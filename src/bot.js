2cec346a8173b7c36352edf3c10795e97b518f77
Looda <31002753+Looda@users.noreply.github.com>
2017-08-14 22:31:21 +0200

    Indent 4spaces, unify definitions, unify response, separate menu getters

--- src/bot.js
+++ src/bot.js
@@ -7,204 +7,233 @@ const moment = require('moment');
 const Bot = require('slackbots');
 
 class LunchBot extends Bot {
-  constructor(settings){
-    super(settings);
-    this.name = 'lunchbot';
-    this.user = null;
-    this.fb_token = settings.fb_token;
-    this.zomato_token = settings.zomato_token;
-  }
-  handleOnStart() {
-    var self = this;
-    this.user = this.users.filter(function (user) {
-      return user.name === self.name;
-    })[0];
-  }
-  handleOnMessage(message){
-    if (this._isChannelMessage(message) &&
-        !this._isFromLunchBot(message))
-    {
-      this._checkMessageContent(message);
+    constructor(settings) {
+        super(settings);
+        this.name = 'lunchbot';
+        this.user = null;
+        this.fb_token = settings.fb_token;
+        this.zomato_token = settings.zomato_token;
+        this.restaurants = [
+            //Ova
+            {
+                keyWord: 'basta',
+                url: 'http://www.pustkoveckabasta.cz/denni-menu/',
+                response: 'Basta:',
+                type: 'basta'
+            },
+            {
+                keyWord: 'jarosi',
+                url: 'http://www.ujarosu.cz/cz/denni-menu/',
+                response: 'Jarosi:',
+                type: 'jarosi'
+            },
+            {
+                keyWord: 'kovork',
+                url: `https://graph.facebook.com/v2.10/kavarnakovork/feed?access_token=${this.fb_token}`,
+                response: 'Kovork:',
+                type: 'kovork'
+            },
+            //NJ
+            {
+                keyWord: 'daniela',
+                url: this.getZomatoUrl('16513150'),
+                response: 'Daniela:',
+                type: 'zomato'
+            },
+            {
+                keyWord: 'art',
+                url: this.getZomatoUrl('16513855'),
+                response: 'Art Cafe:',
+                type: 'zomato'
+            },
+            {
+                keyWord: 'nano',
+                url: this.getZomatoUrl('16513503'),
+                response: 'Pizzeria Nano:',
+                type: 'zomato'
+            },
+            {
+                keyWord: 'cech',
+                url: this.getZomatoUrl('16525386'),
+                response: 'Cechovní dům:',
+                type: 'zomato'
+            },
+        ];
     }
-  }
-  _isChannelMessage(message){
-    return (message.type === 'message') &&
-           Boolean(message) &&
-           (typeof(message.channel) === 'string') &&
-           (message.channel[0] === 'C' ||
-            message.channel[0] === 'G' ||
-            message.channel[0] === 'D');
-  }
-  _isFromLunchBot(message){
-    return message.user === this.user.id;
-  }
-  _checkMessageContent(message){
-    const self = this;
-    const restaurants = [
-      //Ova
-      'basta', 'jarosi', 'kovork',
-      //NJ
-      'daniela', 'art', 'nano', 'cech'
-    ];
-    for (var restaurant_name of restaurants) {
-      if(message.text.indexOf(`:${restaurant_name}:`) > -1){
-        self._getMenu(restaurant_name).then(function(menu){
-          self._replyToMessage(message, menu);
-        }).catch(function(e){
-          console.log(e);
-        });
-      }
+
+    handleOnStart() {
+        const myName = this.name;
+        this.user = this.users.filter(
+            (user) => user.name === myName
+        )[0];
     }
-  }
 
-  _replyToMessage(originalMessage, response){
-    this.postMessage(originalMessage.channel, response, { as_user: true })
-  }
-
-  _getMenu(restaurant_name){
-    var self = this;
-    var url;
-    switch (restaurant_name) {
-      case 'basta':
-        url = 'http://www.pustkoveckabasta.cz/denni-menu/';
-        return new Promise(function(resolve, reject) {
-          request(url, function(e, r, html){
-            if(!e){
-              const $ = cheerio.load(html);
-              var menu = "Sorry, I couldn't find menu for today.";
-              $('.title').each(function(){
-                if($(this).text().indexOf(moment().format('D. MM. YYYY')) > -1){
-                  menu = "Basta: \n ```";
-                  $(this).siblings().each(function(){
-                    menu += $(this).text().replace(/\s+/g, ' ').trim();
-                    menu += "\n";
-                  });
-                  menu += '```';
-                }
-              });
-              resolve(menu);
-            }else{
-              reject(e);
-            }
-          });
-        });
-      case 'jarosi':
-        url = 'http://www.ujarosu.cz/cz/denni-menu/';
-        return new Promise(function(resolve, reject) {
-          request(url, function(e, r, html){
-            if(!e){
-              const $ = cheerio.load(html);
-              var days = ["Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek"];
-              var today = moment().day() - 1;
-              var found = 0;
-              var menu = "";
-              $('tr').each(function(i, el){
-                let text = $(this).text().replace(/\n/g, '').replace(/\s+/g, ' ').trim();
-                if(found == 0 && text.indexOf(days[today]) == 0){
-                  found = 1
-                  menu = "Jarosi: \n ```";
-                  menu += text.trim();
-                } else if (found > 0){
-                  if(text.indexOf(days[today+1]) > -1 || found > 5){
-                    return;
-                  } else {
-                    menu += '\n';
-                    menu += text;
-                    found++;
-                  }
-                }
-              });
-              if (found > 0){
-                menu += '```';
-              }else{
-                menu = "Sorry, I couldn't find menu for today.";
-              }
-              resolve(menu);
-            }else{
-              reject(e);
-            }
-          });
-        });
-      case 'kovork':
-        url = `https://graph.facebook.com/v2.10/kavarnakovork/feed?access_token=${self.fb_token}`;
-        return new Promise(function(resolve, reject) {
-          request(url, function(e, r, html){
-            if(!e){
-              var menu = "Sorry, I couldn't find menu for today.";
-              var json = JSON.parse(html);
-              if(json.data){
-                for( var i = 0; i < 7; i++){
-                  var text = decodeURIComponent(json.data[i].message);
-                  if (text.indexOf(moment().format('D.M.YYYY')) > -1){
-                    menu = "Kovork: \n ```"
-                    menu += text.replace(/\n\n/g, '\n');
-                    menu += '```';
-                  }
-                }
-              }
-              resolve(menu);
-            }else{
-              reject(e);
-            }
-          });
-        });
-      case 'daniela':
-      case 'art':
-      case 'nano':
-      case 'cech':
-        return self._getZomatoMenu(restaurant_name);
-    }
-  }
-
-  _getZomatoMenu(keyword){
-    const self = this;
-    const rest_info = {
-      'daniela': {
-        'id': '16513150',
-        'name': 'Daniela'
-      },
-      'art': {
-        'id': '16513855',
-        'name': 'Art Cafe'
-      },
-      'nano': {
-        'id': '16513503',
-        'name': 'Pizzeria Nano'
-      },
-      'cech': {
-        'id': '16525386',
-        'name': 'Cechovní dům'
-      }
-    }[keyword];
-    const url = `https://developers.zomato.com/api/v2.1/dailymenu?res_id=${rest_info.id}`;
-    return new Promise(function(resolve, reject) {  
-      request({
-        url: url,
-        headers: { 'user_key' : self.zomato_token }
-      }, function(e, r, html){
-        if(!e){
-          var json = JSON.parse(html);
-          var menu = "Sorry, I couldn't find menu for today.";
-          if(json['daily_menus']){
-            for (var daily_menu of json['daily_menus']){
-              if(moment().isSame(daily_menu['daily_menu']['start_date'], 'day') &&
-                 daily_menu['daily_menu']['end_date'] !== undefined){
-                  menu = `${rest_info.name}:` + "\n ```";
-                  for(var dish of daily_menu['daily_menu']['dishes']){
-                    menu += dish['dish']['name'];
-                    menu += '\n';
-                  }
-                  menu += '```';
-              }
-            }
-          }
-          resolve(menu);
-        }else{
-          reject(e);
+    handleOnMessage(message) {
+        if (this.isChannelMessage(message) &&
+            !this.isFromLunchBot(message)) {
+            this.checkMessageContent(message);
         }
-      });
-    });
-  }
+    }
+
+    isChannelMessage(message) {
+        return (message.type === 'message') &&
+            Boolean(message) &&
+            (typeof (message.channel) === 'string') && ['C', 'G', 'D'].includes(message.channel[0]);
+    }
+
+    isFromLunchBot(message) {
+        return message.user === this.user.id;
+    }
+
+    checkMessageContent(message) {
+        this.restaurants.map((restaurant) => {
+            const restaurant_name = restaurant.keyWord;
+            if (message.text && message.text.indexOf(`:${restaurant_name}:`) > -1) {
+                this.getMenu(restaurant)
+                    .then((menu) => this.createResponse(restaurant, menu))
+                    .then((response) => this.replyToMessage(message, response))
+                    .catch((e) => {
+                        console.log(e);
+                    });
+            }
+        });
+    }
+
+    createResponse(restaurant, menu) {
+        if (menu) {
+            return `${restaurant.response} \n\`\`\`${menu}\`\`\``;
+        } else {
+            return `${restaurant.response} Sorry, I couldn't find menu for today.`;
+        }
+    }
+
+    replyToMessage(originalMessage, response) {
+        this.postMessage(originalMessage.channel, response, {
+            as_user: true
+        })
+    }
+
+    getMenu(restaurant) {
+        switch (restaurant.type) {
+            case 'basta': return this.getBasta(restaurant);
+            case 'jarosi': return this.getJarosi(restaurant);
+            case 'kovork': return this.getKovork(restaurant);
+            case 'zomato': return this.getZomato(restaurant);
+        }
+    }
+
+    getBasta(restaurant) {
+        return new Promise(function (resolve, reject) {
+            request(restaurant.url, function (e, r, html) {
+                if (!e) {
+                    const $ = cheerio.load(html);
+                    let menu = [];
+                    $('.title').each(function () {
+                        if ($(this).text().indexOf(moment().format('D. MM. YYYY')) > -1) {
+                            $(this).siblings().each(function () {
+                                menu.push($(this).text().replace(/\s+/g, ' ').trim());
+                            });
+                        }
+                    });
+                    resolve(menu.join("\n"));
+                } else {
+                    reject(e);
+                }
+            });
+        });
+    }
+
+    getJarosi(restaurant) {
+        return new Promise(function (resolve, reject) {
+            request(restaurant.url, function (e, r, html) {
+                if (!e) {
+                    const $ = cheerio.load(html);
+                    const days = ["Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek"];
+                    const today = moment().day() - 1;
+                    let menu = [];
+                    $('tr').each(function (i, el) {
+                        let text = $(this).text().replace(/\n/g, '').replace(/\s+/g, ' ').trim();
+                        if (!menu.length && text.indexOf(days[today]) == 0) {
+                            menu.push(text.trim());
+                        } else if (menu.length) {
+                            if (text.indexOf(days[today + 1]) > -1 || menu.length > 5) {
+                                return;
+                            } else {
+                                menu.push(text);
+                            }
+                        }
+                    });
+                    if (menu.length) {
+                        resolve(menu.join("\n"));
+                    } else {
+                        resolve();
+                    }
+                } else {
+                    reject(e);
+                }
+            });
+        });
+    }
+
+    getKovork(restaurant) {
+        return new Promise(function (resolve, reject) {
+            request(restaurant.url, function (e, r, html) {
+                if (!e) {
+                    var json = JSON.parse(html);
+                    if (json.data) {
+                        let menu = "";
+                        for (var i = 0; i < 7; i++) {
+                            var text = decodeURIComponent(json.data[i].message);
+                            if (text.indexOf(moment().format('D.M.YYYY')) > -1) {
+                                menu += text.replace(/\n\n/g, '\n');
+                            }
+                        }
+                        resolve(menu);
+                    } else {
+                        resolve();
+                    }
+                } else {
+                    reject(e);
+                }
+            });
+        });
+    }
+
+    getZomatoUrl(id) {
+        return `https://developers.zomato.com/api/v2.1/dailymenu?res_id=${id}`;
+    }
+
+    getZomato(restaurant) {
+        const self = this;
+        return new Promise(function (resolve, reject) {
+            request({
+                url: restaurant.url,
+                headers: {
+                    'user_key': self.zomato_token
+                }
+            }, function (e, r, html) {
+                if (!e) {
+                    var json = JSON.parse(html);
+                    if (json['daily_menus']) {
+                        let menu = [];
+                        for (var daily_menu of json['daily_menus']) {
+                            if (moment().isSame(daily_menu['daily_menu']['start_date'], 'day') &&
+                                daily_menu['daily_menu']['end_date'] !== undefined) {
+                                for (var dish of daily_menu['daily_menu']['dishes']) {
+                                    menu.push(dish['dish']['name']);
+                                }
+                            }
+                        }
+                        resolve(menu.join("\n"));
+                    } else {
+                        resolve();
+                    }
+                } else {
+                    reject(e);
+                }
+            });
+        });
+    }
 }
 
 module.exports = LunchBot;
